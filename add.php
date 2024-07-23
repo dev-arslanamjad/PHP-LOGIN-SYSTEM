cb747f536e31bbf42863e483104bf5653821a3ff
Arslan Amjad <arslanamjad4444@gmail.com>
2024-07-23 13:25:18 +0500

    added 2 new coloums

--- add.php
+++ add.php
@@ -1,32 +1,39 @@
 <?php
 session_start();
 include('config.php');
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
+
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$email = mysqli_real_escape_string($conn, $_POST['email']);
+$password = mysqli_real_escape_string($conn, $_POST['password']);
+$cpassword = mysqli_real_escape_string($conn, $_POST['cpassword']);
 $_SESSION["thepassword"] = $password;
-$cpassword = $_POST['cpassword'];
 if ($password == $cpassword) {
     $sql = "SELECT * FROM user WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
-    if ($email !== $row["email"]) {
+
+    if ($email == $row["email"]) {
         $longpassword = md5($password);
-        $sql = "INSERT INTO user (name, email, password) VALUES ('$name', '$email', '$longpassword')";
+        $v_code = bin2hex(random_bytes(16));
+        $sql = "INSERT INTO user (name, email, password, `verification-code`, is_varified) VALUES ('$name', '$email', '$longpassword', '$v_code', '')";
+        
         if ($conn->query($sql) === TRUE) {
             $sql = "SELECT * FROM user WHERE email = '$email'";
             $result = mysqli_query($conn, $sql);
             $row = mysqli_fetch_array($result);
-            $_SESSION["thename"] =  $row['name'];
-            $_SESSION["theemail"] =  $row['email'];
-            $_SESSION["theemail"] =  $row['email'];
+
+            $_SESSION["thename"] = $row['name'];
+            $_SESSION["theemail"] = $row['email'];
             $_SESSION['thepassword'] = $password;
+
             header("location: email.php");
+        } else {
+            echo "<script>alert('Error: " . $conn->error . "');window.location.href='regform.php';</script>";
         }
     } else {
-        echo "<script>alert('Email Already Exists try another')</script>";
+        echo "<script>alert('Email already exists, try another');window.location.href='regform.php';</script>";
     }
 } else {
-    echo "<script>alert('Password Do not matched');window.location.href='regform.php';</script>";
+    echo "<script>alert('Passwords do not match');window.location.href='regform.php';</script>";
 }
 ?>
