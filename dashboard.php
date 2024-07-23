bde4c0a1b8c7288d61213d341f68d29bc117f15c
Arslan Amjad <arslanamjad4444@gmail.com>
2024-07-23 17:40:40 +0500

    minor updates to limitize the unvarified user

--- dashboard.php
+++ dashboard.php
@@ -57,13 +57,34 @@ session_start();
             </div>
         </nav>
     </div>
-    <div class="container hero">
+    <div class="container hero mt-4">
         <h1 class="text-dark">Welcome <span class="text-primary">
                 <?php echo $_SESSION["thename"]; ?>
             </span></h1>
     </div>
     <div class="container">
-
+<!-- Hero 6 - Bootstrap Brain Component -->
+<section class="py-4 py-md-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="container-fluid bsb-hero-6 bsb-overlay p-3 rounded" style="--bsb-overlay-opacity: 0.5; --bsb-overlay-bg-color: var(--bs-light-rgb); background-image: url('223010585_470077960827263_7676708280246605941_n.jpg');">
+          <div class="row justify-content-md-center align-items-center">
+            <div class="col-12 col-md-11 col-xl-10">
+              <h2 class="display-1 text-center text-md-start text-shadow-head fw-bold mb-4">Welcome to Presta</h2>
+              <p class="lead text-center text-md-start text-shadow-body mb-5 d-flex justify-content-sm-center justify-content-md-start">
+                <span class="col-12 col-sm-10 col-md-8 col-xxl-7">Where every squeak, every rattle, and every wobble finds its solution, ensuring your ride is always smooth and worry-free.</span>
+              </p>
+              <div class="d-grid gap-2 d-sm-flex justify-content-sm-center justify-content-md-start">
+                <a href="#!" class="btn bsb-btn-2xl btn-outline-dark">Explore More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
